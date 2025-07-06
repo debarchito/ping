4c4d3e047890267266597aafbf813b38378a0471
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-07-06 09:46:17 +0530

    Run backend in https mode

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,21 @@
 import { handler } from "../build/handler.js";
-import { createServer } from "node:http";
+import { createServer } from "node:https";
 import injectSocketIO from "./io.ts";
 import express from "express";
+import fs from "node:fs";
 
 const app = express();
-const server = createServer(app);
+const server = createServer(
+  {
+    key: fs.readFileSync(`${import.meta.dirname}/../cert/key.pem`),
+    cert: fs.readFileSync(`${import.meta.dirname}/../cert/cert.pem`),
+  },
+  app,
+);
+
 const port = Number(process.env.PING_PORT) || 3000;
 const host = process.env.PING_HOST || "0.0.0.0";
 
 injectSocketIO(server);
 app.use(handler);
-server.listen(port, host, () => console.log(`[?] Listening on http://${host}:${port}`));
+server.listen(port, host, () => console.log(`[?] Listening on https://${host}:${port}`));
