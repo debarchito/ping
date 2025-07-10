789902f67f9e5df048e9db0e6ed5b7dd5bae21b9
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-07-10 13:24:27 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-node";
 
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     csrf: {
+      // Implemented manually in src/hooks.server.ts
       checkOrigin: false,
     },
   },
